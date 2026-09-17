fbfd3ad95e473c9badcc829b613bc7ffd6cac1ec
seraph-games <nadiakhodak2@proton.me>
2026-09-17 14:28:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,7 @@
 const CACHE_NAME = 'cache-v2';
 const urlsToCache = [
   '/index.html',
-  '/robots.txt',
-  '/manifest.json',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
+  '/app.js?v=2.1',
 ];
 
 self.addEventListener('install', event => {
